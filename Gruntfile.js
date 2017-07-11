aafff5e375127f3c170cd6c636d85a864d3edd0e
Christoffer Jansson <cjansss@gmail.com>
2017-07-11 12:51:53 +0200

    Add eslint and deps + fix css errors

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/* globals module */
+
+module.exports = function(grunt) {
+  // configure project
+  grunt.initConfig({
+    // make node configurations available
+    pkg: grunt.file.readJSON('package.json'),
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: ['css/*.css', 'src/css/*.css']
+    },
+    eslint: {
+      options: {
+        configFile: '.eslintrc',
+        cache: true
+      },
+      target: ['src/**/*.js']
+    },
+    githooks: {
+      all: {
+        'pre-commit': 'csslint htmlhint eslint'
+      }
+    },
+    htmlhint: {
+      html1: {
+        src: [
+          'src/**/*.html, index.html'
+        ]
+      }
+    },
+  });
+
+  // enable plugins
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-githooks');
+  grunt.loadNpmTasks('grunt-htmlhint');
+
+  // set default tasks to run when grunt is called without parameters
+  grunt.registerTask('default', ['csslint', 'htmlhint', 'eslint']);
+};
